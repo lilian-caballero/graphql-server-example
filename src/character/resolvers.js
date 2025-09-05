376de0fccbd1016565189743c285bedd05c6d0ba
lilian-caballero <lilian.pereira1620@gmail.com>
2025-09-05 14:16:08 -0300

    feat(character): implementing full crud

--- src/character/resolvers.js
+++ src/character/resolvers.js
@@ -1,4 +1,5 @@
 const db = require('../_db');
+const fs = require('fs');
 
 module.exports = {
     Query: {
@@ -7,5 +8,59 @@ module.exports = {
     },
     Character: {
         books: (character) => db.books.filter((book) => character.bookIds.includes(book.id))
+    },
+    Mutation: {
+        deleteCharacter: (_, { id }) => {
+            let characters = db.characters.filter((character) => character.id != id);
+            let path = __dirname.replace('character', '/_db.js');
+
+            db.characters = characters;
+
+            fs.writeFileSync(
+                path,
+                `module.exports = ${JSON.stringify(db, null, 2)};`,
+                'utf-8'
+            );
+
+            return characters;
+        },
+        addCharacter: (_, addCharacterInput) => {
+            let character = {
+                id: Math.floor(Math.random() * 10000),
+                ...addCharacterInput.input
+            }
+            let path = __dirname.replace('character', '/_db.js');
+
+            db.characters.push(character);
+
+            fs.writeFileSync(
+                path,
+                `module.exports = ${JSON.stringify(db, null, 2)};`,
+                'utf-8'
+            );
+
+            return character;
+        },
+        updateCharacter: (_, updateCharacterInput) => {
+            let id = updateCharacterInput.id;
+            let characters = db.characters.map((character) => {
+                if (character.id == id) {
+                    return { ...character, ...updateCharacterInput.input };
+                }
+
+                return character;
+            });
+            let path = __dirname.replace('character', '/_db.js');
+
+            db.characters = characters;
+
+            fs.writeFileSync(
+                path,
+                `module.exports = ${JSON.stringify(db, null, 2)}`,
+                'utf-8'
+            );
+
+            return characters.find((character) => character.id == id);
+        }
     }
 };
--- src/character/types.js
+++ src/character/types.js
@@ -5,11 +5,29 @@ module.exports = gql`
     id: ID!,
     name: String!,
     age: Int,
-    books: [Book]!
+    books: [Book!]!
   }
 
   extend type Query {
     characters: [Character],
     character(id: ID!): Character
+  }
+
+  extend type Mutation {
+    deleteCharacter(id: ID!): [Character]
+    addCharacter(input: AddCharacterInput!): Character,
+    updateCharacter(id: ID!, input: EditCharacterInput!): Character
+  }
+
+  input AddCharacterInput {
+    name: String!,
+    age: Int,
+    bookIds: [ID!]!
+  }
+
+  input EditCharacterInput {
+    name: String,
+    age: Int,
+    bookIds: [ID!]
   }
 `;
